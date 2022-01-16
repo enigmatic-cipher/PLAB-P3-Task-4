cc48995c8b701fd72970b611b99565c4a54a519f
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-01-16 12:42:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Given n as input. Print following pattern using For loop.
+n = 2
+Expected Output:
+#
+#
+2
+1
+"""
+
+n = 2
+pt = "#"
+for i in range(1, n+1):
+  print(pt)
+for i in range(n, 0, -1):
+  print(i)
